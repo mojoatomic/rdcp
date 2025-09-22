b3b8cd212bdea3c4b720830221b45ba1117292d4
Doug Fennell <dougfennell@Dougs-MacBook-Pro.local>
2025-09-22 17:29:44 -0500

    test(jwks): remove Node<18 skip; ci: fallback to local install if version not yet published; release: bump to 2.0.0

--- tests/jwks.client.cache.e2e.test.ts
+++ tests/jwks.client.cache.e2e.test.ts
@@ -61,10 +61,7 @@ function startServer(): Promise<{
   })
 }
 
-const __nodeMajor = parseInt(process.versions.node.split('.')[0], 10)
-const __maybeDescribe = __nodeMajor < 18 ? describe.skip : describe
-
-__maybeDescribe('JWKS client cache (ttlMs + ETag/304)', () => {
+describe('JWKS client cache (ttlMs + ETag/304)', () => {
   let baseUrl = ''
   let closeServer: () => Promise<void>
   let getHits: () => number
